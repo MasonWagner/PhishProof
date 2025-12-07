ceccadee5962eb9f79d30f9c1c4c96cf557c4fdc
Mason Wagner <mwags251@gmail.com>
2025-12-07 00:38:33 -0600

    Home page creation, navigation created, CSS updated.

--- phishproofapi/urls.py
+++ phishproofapi/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('class_list/', views.class_list, name='class_list'),
     path('class/<int:pk>/', views.class_description, name='class_description'),
 ]
--- phishproofapi/views.py
+++ phishproofapi/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from .models import Classes
 
 # Create your views here.
+def home(request):
+    return render(request, 'phishproofapi/home.html', {'home': home})
 def class_list(request):
     class_list = Classes.objects.all()
     return render(request, 'phishproofapi/class_list.html', {'class_list': class_list})
